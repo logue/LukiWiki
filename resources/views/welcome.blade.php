073a25fa61761c3eb220ea16e4cb959d97307be6
Logue <logue@hotmail.co.jp>
2018-01-07 14:00:52 +0900

    テンプレートのIF文ミスの修正。csrfトークンのメタタグを追記。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,28 +3,28 @@
 
 <head>
 	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="csrf-token" content="{{ csrf_token() }}" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>{{ config('app.name') }}</title>
 	<link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
-	<!--[if IE]>
-      <link href="{{ asset('css/bootstrap-ie9.css') }}" rel="stylesheet" />
-      <script src="{{ asset('js/html5shiv.min.js') }}"></script>
-    <![endif]-->
+<!--[if IE]>
+	<link href="{{ asset('css/bootstrap-ie9.css') }}" rel="stylesheet" />
+	<script src="{{ asset('js/html5shiv.min.js') }}"></script>
+<![endif]-->
 	<!--[if lt IE 9]>
-	  <link href="{{ asset('css/bootstrap-ie8.css') }}" rel="stylesheet" />
-    <![endif]-->
+	<link href="{{ asset('css/bootstrap-ie8.css') }}" rel="stylesheet" />
+<![endif]-->
 </head>
 
 <body>
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 		<a class="navbar-brand" href="#">{{ config('app.name') }}</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar-collapse"
 		 aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+		<div class="collapse navbar-collapse" id="navbar-collapse">
 			@if (Route::has('login'))
 			<ul class="navbar-nav mr-auto">
 				@auth
@@ -42,21 +42,21 @@
 				</li>
 				@endauth
 			</ul>
-			<form class="form-inline my-2 my-lg-0">
+			@endif
+			<form class="form-inline my-2 my-lg-0 mr-0">
 				<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 			</form>
 		</div>
-		@endif
 	</nav>
-	<main role="main" class="container">
+
+	<main role="main" class="container" id="app">
 		<div class="jumbotron text-center mt-3">
 			<h1 class="display-4">Laravel</h1>
 			<p class="display-1">
 				<i class="fab fa-laravel"></i>
 			</p>
 			<p class="lead">Laravel + Bootstrap4 + Font Awasome Test.</p>
-
 		</div>
 
 		<ul class="list-inline">
